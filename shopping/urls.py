5a7287babec430c99b7578c0b8994c05d37e1515
Md Roni Ahamed <mdroni0193101@gmail.com>
2023-02-22 16:13:02 +0600

    cart Product add

--- shopping/urls.py
+++ shopping/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('checkout/',views.check_out,name='checkout'),
-    path('confirmation/',views.confirmation, name='confirmation')
+    path('confirmation/',views.confirmation, name='confirmation'),
+    path('cart/',views.cart, name='cart')
+    
 ]
