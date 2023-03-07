037bab51d832f16e62dd8e003e1c58e53b5d64d8
Md Roni Ahamed <mdroni0193101@gmail.com>
2023-03-07 12:55:39 +0600

    add

--- Furniture_Bazar/settings.py
+++ Furniture_Bazar/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from curses.ascii import EM
 from pathlib import Path
 import os
 
@@ -31,7 +32,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -49,7 +49,9 @@ INSTALLED_APPS = [
     'store',
     'contact',
     'searching',
-    'about'
+    
+    'about',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -89,7 +91,7 @@ WSGI_APPLICATION = 'Furniture_Bazar.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'furniture_bazar',
+        'NAME': 'furniture',
         'USER': 'root',
         'PASSWORD':'',
         'HOST': 'localhost',
@@ -141,6 +143,13 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'Furniture_Bazar/static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMIL_HOST_USER = 'mdroniahamed56@gmail.com'
+EMAIL_HOST_PASSWORD = 'peffmbhxcpwjctxt'
+EMAIL_USE_TLS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
